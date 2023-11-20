4cdec4d64a373659f53b8b168e0242b710ba4626
printf_cherif <printf0cherif@gmail.com>
2023-11-20 13:45:42 +0000

    Implementation de la modification et de la suppression

--- 04/app.js
+++ 04/app.js
@@ -87,9 +87,9 @@ app.get("/", async (requete, reponse) => {
     let page = "<!DOCTYPE html><html><head><meta charset='utf-8'><title>MySQL Connexion</title></head><body><a href='/'>Liste des comptes</a> <a href='/formulaire'>Création d'un compte</a>";
     let donnees = await listCompte();
 
-    page = page.concat("<table><tr><th>Numero compte</th><th>Prenom</th><th>Nom</th><th>Solde</th></tr>");
+    page = page.concat("<table><tr><th>Numero compte</th><th>Prenom</th><th>Nom</th><th>Solde</th><th>Actions</th></tr>");
     donnees.forEach(donnee => {
-        page = page.concat("<tr><td>" + donnee.numcompte + "</td><td>" + donnee.prenom + "</td><td>" + donnee.nom + "</td><td>" + donnee.solde + "</td></tr>");
+        page = page.concat("<tr><td>" + donnee.numcompte + "</td><td>" + donnee.prenom + "</td><td>" + donnee.nom + "</td><td>" + donnee.solde + "</td><td><a href='/formulaire-modification/" + donnee.numcompte + "'>Modifier</a><form method='post' action='/supprimer/" + donnee.numcompte + "'><input type='submit' value='Supprimer'></form></td></tr>");
     });
     page = page.concat("</table></body></html>");
 
@@ -105,6 +105,15 @@ app.get("/formulaire", (requete, reponse) => {
     reponse.send(page);
 });
 
+// Définition de la route pour la modification "/formulaire-modification" pour afficher un formulaire de modification de compte
+app.get("/formulaire-modification/:numcompte", (requete, reponse) => {
+    let numcompte = requete.params.numcompte;
+    // Construction de la page HTML avec un formulaire
+    let page = "<!DOCTYPE html><html><head><meta charset='utf-8'><title>MySQL Connexion</title></head><body>";
+    page = page.concat("<a href='/'>Liste des comptes</a> <a href='/formulaire'>Création d'un compte</a> <form method='post' action='/miseajour/" + numcompte + "'>Numero du compte<input type='text' value='" + numcompte + "'><br> Nouveau solde<input name='nouveauSolde' type='text'><br><input type='submit' value='Modifier'></form></body></html>");
+    reponse.send(page);
+});
+
 // Définition de la route POST "/comptes" pour traiter le formulaire de création de compte
 app.post("/comptes", (requete, reponse) => {
     // Appel de la fonction pour créer un compte avec les données du formulaire
@@ -113,7 +122,23 @@ app.post("/comptes", (requete, reponse) => {
     reponse.redirect("/");
 });
 
-// Creer les autres routes pour la modification et la suppression
+// Définition de la route POST "/miseajour" pour traiter le formulaire de modification de compte
+app.post("/miseajour/:numcompte", (requete, reponse) => {
+    // Appel de la fonction pour créer un compte avec les données du formulaire
+    updateCompte(requete.params.numcompte, requete.body.nouveauSolde);
+    // Redirection vers la route "/"
+    reponse.redirect("/");
+});
+
+// Définition de la route POST "/miseajour" pour traiter le formulaire de modification de compte
+app.post("/supprimer/:numcompte", (requete, reponse) => {
+    let numcompte = requete.params.numcompte;
+    // Appel de la fonction pour créer un compte avec les données du formulaire
+    deleteCompte(numcompte);
+    // Redirection vers la route "/"
+    reponse.redirect("/");
+});
+
 
 // Démarrage du serveur Express
 app.listen(port, () => {
