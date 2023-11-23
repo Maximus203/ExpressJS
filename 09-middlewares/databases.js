86775fe6446e9ffc0e31df401f00b51014176237
printf_cherif <printf0cherif@gmail.com>
2023-11-23 20:07:48 +0000

    MySQL connexion

---
+++ 09-middlewares/databases.js
@@ -0,0 +1,85 @@
+// Importation des modules nécessaires
+const mysql = require('mysql2');
+
+// Connexion à la base de données MySQL
+const connexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'express',
+    database: 'banque'
+});
+
+// Fonction asynchrone qui retourne une promesse pour lister les comptes de la base de données
+function listCompte() {
+    return new Promise((resolve, reject) => {
+        // Requête SQL pour sélectionner tous les enregistrements de la table 'compte'
+        connexion.query("SELECT * FROM compte",
+            (erreurs, resultats, champs) => {
+                if (erreurs) {
+                    reject(erreurs);
+                }
+                else {
+                    resolve(resultats);
+                }
+            });
+    });
+}
+
+// Fonction pour créer un compte dans la base de données
+function createCompte(numcompte, prenom, nom, solde) {
+    return new Promise((resolve, reject) => {
+        // Requête SQL pour l'insertion d'un nouvel enregistrement dans la table 'compte'
+        connexion.query("INSERT INTO compte(numcompte, prenom, nom, solde) VALUES(?,?,?,?)",
+            [numcompte, prenom, nom, solde],
+            (erreurs, resultats, champs) => {
+                if (erreurs) {
+                    reject(erreurs);
+                }
+                else {
+                    resolve(resultats);
+                }
+            });
+    });
+
+}
+
+// Fonction pour modifier le solde d'un compte dans la base de données
+function updateCompte(numcompte, nouveauSolde) {
+    return new Promise((resolve, reject) => {
+        // Requête SQL pour mettre à jour le solde d'un enregistrement dans la table 'compte'
+        connexion.query("UPDATE compte SET solde = ? WHERE numcompte = ?",
+            [nouveauSolde, numcompte],
+            (erreurs, resultats, champs) => {
+                if (erreurs) {
+                    reject(erreurs);
+                }
+                else {
+                    resolve(resultats);
+                }
+            });
+    });
+}
+
+// Fonction pour supprimer un compte de la base de données
+function deleteCompte(numcompte) {
+    return new Promise((resolve, reject) => {
+        // Requête SQL pour supprimer un enregistrement de la table 'compte'
+        connexion.query("DELETE FROM compte WHERE numcompte = ?",
+            [numcompte],
+            (erreurs, resultats, champs) => {
+                if (erreurs) {
+                    reject(erreurs);
+                }
+                else {
+                    resolve(resultats);
+                }
+            });
+    });
+}
+
+// Exportation des fonctions pour les rendre accessibles depuis d'autres fichiers
+module.exports = {
+    listCompte,
+    createCompte,
+    updateCompte,
+    deleteCompte
+}
